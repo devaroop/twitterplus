cc12b4fc75122d26b24ea1e5df6eeb86c9c08887
Devaroop <devaroop@devbantu.(none)>
2012-09-14 11:38:00 +0530

    first commit for teja-hotels code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HotelsTeja::Application.config.secret_token = '1b78fed1dfb6a8040926b8ef3c070f7d634242270a6ddc6babefca2ffcf0cf62dc867b568d1ddcdd7d0b9bbc97b3bf98c79245846d39d18ff1ec811ad7788705'
